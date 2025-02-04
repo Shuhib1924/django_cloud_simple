ef67783996908171f74169fa5458f04f545ef6c2
Shuhib <shuhib_s@live.de>
2025-02-04 20:52:42 +0100

    simple login

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,17 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def user_login(request):
-    return HttpResponse("logged in")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse("logged in")
+        else:
+            return HttpResponse("user not found")
+    else:
+        return render(request, "accounts/login.html")
