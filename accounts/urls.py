f654a8632cde72cf090f488ef513374a635242f4
Shuhib <shuhib_s@live.de>
2025-02-06 17:12:51 +0100

    modify login from django.contrib.auth

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from accounts import views
+# from accounts import views
 
 app_name = "accounts"
 urlpatterns = [
-    path("login/", views.user_login, name="user_login"),
-    path("logout/", views.user_logout, name="user_logout"),
+    # path("login/", views.user_login, name="user_login"),
+    # path("logout/", views.user_logout, name="user_logout"),
+    path("login/", auth_views.LoginView.as_view(), name="user_login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="user_logout"),
 ]
