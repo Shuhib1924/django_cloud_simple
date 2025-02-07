1603ee989d3d6aa383ba29105ec5d39401f9a314
Shuhib <shuhib_s@live.de>
2025-02-07 18:56:46 +0100

    add registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-# from accounts import views
+from accounts import views
 
 app_name = "accounts"
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     # path("logout/", views.user_logout, name="user_logout"),
     path("login/", auth_views.LoginView.as_view(), name="user_login"),
     path("logout/", auth_views.LogoutView.as_view(), name="user_logout"),
+    path("register/", views.register, name="register"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
@@ -22,3 +23,20 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("cloud:index")
+
+
+def register(request):
+    if request.method == "POST":
+        data = request.POST
+        # print(f"data: {data}")
+        if data["password"] == data["confirm_password"]:
+            user = User.objects.create_user(
+                username=data["username"],
+                email=data["email"],
+                password=data["password"],
+            )
+            login(request, user)
+            return redirect("cloud:index")
+        else:
+            return HttpResponse("passwords don't match")
+    return render(request, "registration/register.html")
