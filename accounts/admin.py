4faa4eb4fb1c85e7c1d80dcfbd9aae25734264c6
Shuhib <shuhib_s@live.de>
2025-02-07 19:24:40 +0100

    add profile with image

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "date_of_birth", "photo")
+    list_filter = ("date_of_birth",)
+    search_fields = ("user__username",)
+
+    # class Meta:
+    #     model = Profile
+    #     fields = ("user", "date_of_birth", "photo")
+
+    # def get_queryset(self, request):
+    #     qs = super().get_queryset(request)
+    #     return qs.filter(user=request.user)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+def profile_path(instance, filename):
+    date_path = datetime.now().strftime("%Y/%m/%d")
+    return f"profile/{instance.user.username}/{date_path}/{filename}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to=profile_path, blank=True)
+
+    def __str__(self):
+        return self.user.username
