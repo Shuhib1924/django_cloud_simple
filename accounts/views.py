be9f3d3af46c75cea6e482f5f0ef16762505d804
Shuhib <shuhib_s@live.de>
2025-02-09 22:36:40 +0100

    extend username and email

--- accounts/views.py
+++ accounts/views.py
@@ -49,9 +49,9 @@ def register(request):
 @login_required
 def profile(request):
     profile = get_object_or_404(Profile, user__username=request.user.username)
-    # user = get_object_or_404(User, username=username)
+    user = get_object_or_404(User, username=request.user.username)
     if request.method == "POST":
-        # data = request.POST
+        print(request.POST)
         # files = request.FILES
         if "date_of_birth" in request.POST:
             profile.date_of_birth = request.POST["date_of_birth"]
@@ -59,10 +59,17 @@ def profile(request):
         if "photo" in request.FILES:
             profile.photo = request.FILES["photo"]
             messages.success(request, "photo updated")
+        if "username" in request.POST:
+            user.username = request.POST["username"]
+            messages.success(request, "username updated")
+        if "email" in request.POST:
+            user.email = request.POST["email"]
+            messages.success(request, "email updated")
         if request.POST["date_of_birth"] == "" and request.FILES["photo"] == "":
             messages.error(request, "nothing to update")
+        user.save()
         profile.save()
         # return redirect("accounts:profile", username=profile.user.username)
         return redirect("accounts:profile")
     else:
-        return render(request, "profile.html", {"profile": profile})
+        return render(request, "profile.html", {"profile": profile, "user": user})
